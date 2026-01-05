12ee7ae264755e750200d1948b42c92bdd80106c
Mateus Andreatta <mateus-henrique2001@hotmail.com>
2026-01-04 21:41:46 -0300

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Mateus Andreatta on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
